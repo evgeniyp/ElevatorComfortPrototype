d85a236d509cc58f4980d300bf76aa3cbe220425
Evgeniy Petukhov <evgeniy.petukhov@gmail.com>
2015-07-16 19:49:04 +0300

    Initial commit

---
+++ WpfApplication1/MainWindow.xaml.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OxyPlot.Series;
+using OxyPlot;
+using System.Windows.Threading;
+using System.IO.Ports;
+
+namespace WpfApplication1
+{
+    public partial class MainWindow : Window
+    {
+        private const int REDRAW_INTERVAL_MS = 30;
+
+        private DispatcherTimer _graphRedrawTimer;
+        private UM6LTSensorParser _parser;
+        private SerialPort _serialPort;
+        private MainViewModel _mainViewModel { get { return DataContext as MainViewModel; } }
+        private long _startTime = DateTime.Now.Ticks;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            InitializeParser();
+            InitializeSerialPort();
+            InitializeRedrawTimer();
+        }
+
+        private void InitializeParser()
+        {
+            _parser = new UM6LTSensorParser();
+        }
+
+        private void InitializeSerialPort()
+        {
+            _serialPort = new SerialPort("COM3");
+
+            var buffer = new byte[16384];
+            _serialPort.Encoding = System.Text.Encoding.ASCII;
+            _serialPort.BaudRate = 115200;
+            _serialPort.Parity = Parity.None;
+            _serialPort.StopBits = StopBits.One;
+            _serialPort.DataBits = 8;
+            _serialPort.Handshake = Handshake.None;
+            _serialPort.DataReceived += (sender, eventArgs) =>
+            {
+                try
+                {
+                    SerialPort sp = (SerialPort)sender;
+                    var bytesToRead = _serialPort.BytesToRead;
+                    sp.Read(buffer, 0, bytesToRead);
+                    _parser.HandleData(buffer, bytesToRead);
+                }
+                catch (Exception e) { this.Dispatcher.BeginInvoke((Action)(() => { this.Title = e.Message; })); }
+            };
+        }
+
+        private void InitializeRedrawTimer()
+        {
+            _graphRedrawTimer = new DispatcherTimer();
+            _graphRedrawTimer.Tick += (sender, e) => { _mainViewModel.Invalidate(); };
+            _graphRedrawTimer.Interval = TimeSpan.FromMilliseconds(REDRAW_INTERVAL_MS);
+        }
+
+        private void EnableDataEntry(bool enable)
+        {
+            if (enable)
+            {
+                _parser.OnX = v => { this.Dispatcher.BeginInvoke((Action)(() => 
+                {
+                    _mainViewModel.AddX((DateTime.Now.Ticks - _startTime) / 10000000.0, v);
+                })); };
+
+                _parser.OnY = v => { this.Dispatcher.BeginInvoke((Action)(() => { 
+                    _mainViewModel.AddY((DateTime.Now.Ticks - _startTime) / 10000000.0, v);
+                })); };
+
+                _parser.OnZ = v => { this.Dispatcher.BeginInvoke((Action)(() => {
+                    _mainViewModel.AddZ((DateTime.Now.Ticks - _startTime) / 10000000.0, v); 
+                })); };
+
+                try
+                {
+                    _serialPort.Open();
+                    _graphRedrawTimer.IsEnabled = true;
+                }
+                catch (System.IO.IOException e) { this.Title = e.Message; }
+            }
+            else
+            {
+                _parser.OnX = null;
+                _parser.OnY = null;
+                _parser.OnZ = null;
+
+                try 
+                {
+                    _graphRedrawTimer.IsEnabled = false;
+                    _serialPort.Close();
+                }
+                catch (System.IO.IOException e) { this.Title = e.Message; }
+            }
+        }
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button.Content.ToString() == "Start")
+            {
+                button.Content = "Stop";
+                EnableDataEntry(true);
+            }
+            else
+            {
+                button.Content = "Start";
+                EnableDataEntry(false);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            EnableDataEntry(false);
+        }
+    }
+
+    public class MainViewModel
+    {
+        private const int COMMAND_RATE = 20;
+        private const int SECONDS_TO_REMEMBER = 10;
+        private const int LOWPASS_FREQ = 5;
+
+        private FilterButterworth _filter = new FilterButterworth(LOWPASS_FREQ, COMMAND_RATE, FilterButterworth.PassType.Lowpass, Math.Sqrt(2));
+
+        public PlotModel MyModel { get; private set; }
+
+        private LineSeries _x = new LineSeries("X");
+        private LineSeries _y = new LineSeries("Y");
+        private LineSeries _z = new LineSeries("Z");
+
+        public MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Accel Data" };
+            this.MyModel.Series.Add(_x);
+            this.MyModel.Series.Add(_y);
+            this.MyModel.Series.Add(_z);
+        }
+
+        public void Invalidate() { MyModel.InvalidatePlot(true); }
+
+        public void AddX(double x, double y)
+        {
+            _x.Points.Add(new DataPoint(x, y));
+            while (_x.Points.Count > COMMAND_RATE * SECONDS_TO_REMEMBER)
+            {
+                _x.Points.RemoveAt(0);
+            }
+        }
+        public void AddY(double x, double y)
+        {
+            _y.Points.Add(new DataPoint(x, y));
+            while (_y.Points.Count > COMMAND_RATE * SECONDS_TO_REMEMBER)
+            {
+                _y.Points.RemoveAt(0);
+            }
+        }
+        public void AddZ(double x, double y)
+        {
+            _filter.Update(y);
+            y = _filter.Value;
+            _z.Points.Add(new DataPoint(x, y));
+            while (_z.Points.Count > COMMAND_RATE * SECONDS_TO_REMEMBER)
+            {
+                _z.Points.RemoveAt(0);
+            }
+        }
+    }
+}
---
+++ WpfApplication1/UM6LTSensorParser.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class PtChar
+{
+    private readonly byte _value;
+    public PtChar(byte value) { _value = Convert.ToByte(value); }
+    public bool HasData()
+    {
+        return (_value & (byte)128) != 0;
+    }
+    public bool IsBatchOperation()
+    {
+        return (_value & (byte)64) != 0;
+    }
+    public int GetDataLength()
+    {
+        return IsBatchOperation() ? (byte)(_value >> 2) & (byte)15 : 1;
+    }
+}
+
+public class UM6LTSensorParser
+{
+    private const float ACCEL_MULTIPLY_FACTOR = 0.000183105f;
+    private const byte UM6_ACCEL_PROC_XY = 0x5E;
+    private const byte UM6_ACCEL_PROC_Z = 0x5F;
+    private readonly byte[] NEW_PACKET_SIGNATURE = { 0x73, 0x6E, 0x70 };
+    private bool _gotFirstSignature = false;
+    private byte[] _buffer = new byte[1024];
+    private int _bufferPosition = 0;
+
+    public Action<float> OnX, OnY, OnZ;
+
+    public void HandleData(byte[] buffer, int bytesToRead)
+    {
+        Array.Copy(buffer, 0, _buffer, _bufferPosition, bytesToRead);
+        _bufferPosition += bytesToRead;
+
+        int snpIndex;
+        do
+        {
+            snpIndex = -1;
+            for (int i = 0; i <= _bufferPosition - NEW_PACKET_SIGNATURE.Length; i++) // looking for new packet signature index
+            {
+                if (_buffer[i + 0] == NEW_PACKET_SIGNATURE[0] &&
+                    _buffer[i + 1] == NEW_PACKET_SIGNATURE[1] &&
+                    _buffer[i + 2] == NEW_PACKET_SIGNATURE[2])
+                {
+                    snpIndex = i;
+                    break;
+                }
+            }
+
+            if (snpIndex > -1)
+            {
+                if (_gotFirstSignature)
+                {
+                    var packet = new byte[snpIndex];
+                    Array.Copy(_buffer, 0, packet, 0, snpIndex); // locating
+                    PacketReceived(packet);
+                }
+                else
+                {
+                    _gotFirstSignature = true; // omitting first incomplete packet
+                }
+
+                // moving buffer to start
+                for (int i = 0; i < _bufferPosition - snpIndex - NEW_PACKET_SIGNATURE.Length; i++)
+                {
+                    _buffer[i] = _buffer[i + snpIndex + NEW_PACKET_SIGNATURE.Length];
+                }
+                // clearing old bytes
+                for (int i = _bufferPosition - snpIndex - NEW_PACKET_SIGNATURE.Length; i < _bufferPosition; i++)
+                {
+                    _buffer[i] = 0x00;
+                }
+                _bufferPosition -= snpIndex + NEW_PACKET_SIGNATURE.Length;
+            }
+        } while (snpIndex > -1);
+    }
+
+    private void PacketReceived(byte[] packet)
+    {
+        //Console.WriteLine("PT: " + Convert.ToString(packet[0], 2).PadLeft(8, '0') + " OTHER: " + BitConverter.ToString(packet, 1, packet.Length - 1));
+
+        PtChar ptChar = new PtChar(packet[0]);
+        if (ptChar.HasData())
+        {
+            byte address = packet[1];
+            if (ptChar.IsBatchOperation())
+            {
+                var dataLength = ptChar.GetDataLength();
+                for (int i = 0; i < dataLength; i++)
+                {
+                    byte[] data = new byte[4];
+                    Array.Copy(packet, 2 + 4 * i, data, 0, 4);
+                    ParseData(address, data);
+                    address++;
+                }
+            }
+            else
+            {
+                byte[] data = new byte[4];
+                Array.Copy(packet, 2, data, 0, 4);
+                ParseData(address, data);
+            }
+        }
+    }
+
+    private void ParseData(byte address, byte[] data)
+    {
+        switch (address)
+        {
+            case UM6_ACCEL_PROC_XY:
+                Int16 integerValueX = (Int16)(((Int16)data[0]) << 8 | data[1]);
+                float floatValueX = (float)integerValueX * ACCEL_MULTIPLY_FACTOR;
+                if (OnX != null) { OnX(floatValueX); }
+
+                Int16 integerValueY = (Int16)(((Int16)data[2]) << 8 | data[3]);
+                float floatValueY = (float)integerValueY * ACCEL_MULTIPLY_FACTOR;
+                if (OnY != null) { OnY(floatValueY); }
+                break;
+            case UM6_ACCEL_PROC_Z:
+                Int16 integerValueZ = (Int16)(((Int16)data[0]) << 8 | data[1]);
+                float floatValueZ = (float)integerValueZ * ACCEL_MULTIPLY_FACTOR;
+                if (OnZ != null) { OnZ(floatValueZ); }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public byte[] GetAccelRequestPacket()
+    {
+        byte[] packet = { NEW_PACKET_SIGNATURE[0], NEW_PACKET_SIGNATURE[1], NEW_PACKET_SIGNATURE[2],
+                          0x00, // packet type
+                          UM6_ACCEL_PROC_Z,
+                          0x00, 0x00 // reserved for checksum
+                        };
+        var checksumBytes = BitConverter.GetBytes(Checksum(packet));
+        packet[packet.Length - 2] = checksumBytes[1];
+        packet[packet.Length - 1] = checksumBytes[0];
+
+        return packet;
+    }
+
+    private ushort Checksum(byte[] bytes)
+    {
+        ushort sum = 0;
+        for (int i = 0; i < bytes.Length; i++)
+        {
+            sum += bytes[i];
+        }
+        return sum;
+    }
+}
